fd7ffa2e7d070a054319d93e6a7d9b1428a2bbe5
chroju <chor.chroju@gmail.com>
2020-05-30 22:13:38 +0900

    Add tests

---
+++ test/providers.tf
@@ -0,0 +1,11 @@
+terraform {
+  backend "remote" {
+    hostname = "app.terraform.io"
+    organization = "chroju"
+
+    workspaces {
+      name = "sample"
+    }
+  }
+  required_version = "~> 0.12.0"
+}
